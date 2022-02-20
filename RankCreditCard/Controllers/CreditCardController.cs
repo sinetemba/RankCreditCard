2c9d718821aebace0d84d325344f5379624435f3
sinetemba <sinetemba101@gmail.com>
2022-02-20 09:58:05 +0200

    first commit

---
+++ RankCreditCard/Controllers/CreditCardController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RankCreditCard.Data;
+using RankCreditCard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankCreditCard.Controllers
+{
+    public class CreditCardController : Controller
+    {
+       
+        private RankCreditCardContext _context;
+
+        public CreditCardController(RankCreditCardContext context)
+        {           
+            _context = context;
+        }
+        public async Task<IActionResult> List()
+        {
+            var creditcards = await _context.CreditCards.ToListAsync();
+            return View(creditcards);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CreditCard creditcard)
+        {
+            //CreditCardValidator.Luhn.CheckLuhn(creditcard.CardNumber);
+
+            //var cc = new CreditCardDetector(creditcard.CardNumber);
+            //var isValid = cc.IsValid();
+            //var brand = cc.Brand;
+
+            //CreditCardValidator.
+
+            // validate that our model meets the requirement
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // update the ef core context in memory 
+                    _context.Add(creditcard);
+
+                    // sync the changes of ef code in memory with the database
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("List");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+            // We return the object back to view
+            return View(creditcard);
+        }
+    }
+}
---
+++ RankCreditCard/Data/Migrations/20220220074259_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RankCreditCard.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CreditCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CardHolderName = table.Column<string>(type: "TEXT", nullable: true),
+                    CardNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    CCVNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpiryYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpiryMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    Provider = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditCards", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditCards");
+        }
+    }
+}
